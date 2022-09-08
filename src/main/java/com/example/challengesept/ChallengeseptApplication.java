8e20b2dc69ee6d5d410c9264e1657b2c2b2c78e2
tania <tania.raastegar@yahoo.com>
2022-09-09 09:44:40 +1000

    Initial commit

---
+++ src/main/java/com/example/challengesept/ChallengeseptApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengesept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeseptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeseptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/challengesept/ChallengeseptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengesept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeseptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
