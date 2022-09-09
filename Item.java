8914d4cdcd6315c89147e615adca7d716f030d5b
taniarastegar <82797864+taniarastegar@users.noreply.github.com>
2022-09-09 17:18:52 +1000

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,61 @@
+package com.example.challengesept.model;
+
+public class Item {
+
+
+    public Item(){
+
+    }
+    private int id;
+    private String name;
+    private String desc;
+    private double price;
+
+    public Item(int id, String name, String desc, double price) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "item{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
